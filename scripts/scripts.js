5539ebbcdb853ecc5d74b1194e5eb5b708607a8f
Michael Cobb <cobbmj@uw.edu>
2018-07-28 23:29:54 -0700

    fixed header todo and made it more responsive

---
+++ scripts/scripts.js
@@ -0,0 +1,9 @@
+//function taken from w3schools Hamburger Menu
+function myFunction() {
+    var x = document.getElementById("myNavbarLeft");
+    if (x.className === "navbarLeft") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbarLeft";
+    }
+}
