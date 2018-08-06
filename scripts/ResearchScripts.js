a2842e91468e506b14b3fe9d2a37ab2d6cc379f1
Michael Cobb <cobbmj@uw.edu>
2018-08-06 00:58:49 -0700

    updated readme and research html

--- scripts/ResearchScripts.js
+++ scripts/ResearchScripts.js
@@ -1,12 +1,7 @@
 //<!--Load the AJAX API-->
 
 // Load the Visualization API and the corechart package.
-google.charts.load('current', {
-  'packages': ['corechart']
-});
-google.charts.load("current", {
-  packages: ["timeline"]
-});
+google.charts.load('current', {'packages': ['corechart']});
 
 // Set a callback to run when the Google Visualization API is loaded.
 google.charts.setOnLoadCallback(drawTotalConsumptionChart);
